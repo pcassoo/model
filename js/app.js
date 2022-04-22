b466d5ed33e50be5b6953a0b14ebaa3adac5a2b6
Pcassoo <paulestinifrancois@gmail.com>
2022-04-22 12:11:52 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Nav toggle
+const navBtn = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
+
+navBtn.addEventListener('click', () => {
+    links.classList.toggle('show-links');
+});
